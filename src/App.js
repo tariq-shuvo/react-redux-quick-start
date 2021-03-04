3d933fb113300ec9c87e229680aeec60997174de
Tariqul <tariq10043@gmail.com>
2021-03-04 11:07:49 +0600

    react router set up is completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, {Fragment} from 'react';
 import './App.css';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
+import Home from './components/layout/Home';
+import Navbar from './components/layout/Navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route
+} from 'react-router-dom'
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => 
-    <Fragment>
-      <Navbar />
-      <Home />  
-    </Fragment> 
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </Fragment> 
+    </Router>
 
 export default App;
